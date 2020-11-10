f3d1ea1324c458e7602cd45c6e83c58bd0d0aa3f
Bekzod Tursunov <amishopogolik@gmail.com>
2020-11-11 01:55:11 +0500

    minor fixes in rspec test

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  let(:user) { create(:user, name: 'Bekzod') }
+  let(:user) { create(:user, name: 'Beka') }
+  let(:another_user) { create(:user, name: 'Bekushka') }
 
     before(:each) do
       assign(:user, user)
@@ -11,15 +12,35 @@ RSpec.describe 'users/show', type: :view do
       render
     end
 
-  it 'user see his name' do
-    expect(rendered).to match('Bekzod')
-  end
+    it 'user see his name' do
+      expect(rendered).to match('Beka')
+    end
 
-  it 'renders change password' do
-    expect(rendered).to match('Сменить имя и пароль')
-  end
+    it 'renders change password' do
+      expect(rendered).to match('Сменить имя и пароль')
+    end
 
-  it 'renders game fragments' do
-    expect(rendered).to match('в процессе')
-  end
+    it 'renders game fragments' do
+      expect(rendered).to match('в процессе')
+    end
+
+
+    before(:each) do
+      assign(:user, another_user)
+      assign(:games, [build_stubbed(:game)])
+
+      render
+    end
+
+    it 'renders second user name' do
+      expect(rendered).to match('Bekushka')
+    end
+
+    it 'renders game fragments' do
+      expect(rendered).to match('в процессе')
+    end
+
+    it 'renders change password' do
+      expect(rendered).not_to match('Сменить имя и пароль')
+    end
 end
